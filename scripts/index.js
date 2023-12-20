f2d2040f2ba1b1b88e6a03533645672ec3939c2e
Einanas <inez.el.azzouzi@hva.nl>
2023-12-20 10:34:34 +0100

    css and javascript fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,23 @@
-//   MENU INTERACTION   //
-let wat = document.querySelector('a:nth-of-type(2)')
+  //   MENU INTERACTION   //
+let interaction = document.querySelectorAll('li')
+console.log(interaction);
 
-wat.addEventListener('click', lightup)
-wat.addEventListener('animationend', lightup)
-
-
-function lightup() {
-  wat.classList.toggle('lightup')
-}
-
-console.log('a:nth-of-type(2)');
-
-
-
-
+interaction.forEach(interaction => {
+  interaction.addEventListener('click', lightup => {
+    interaction.classList.toggle('lightup')
+  })
+});
 
 // In plaats van flipcard, afbeelding laten verschijnen.
 // twee afbeeldingen op één plek
 // opacity met klik
 // met javascritp de klik mogelijk maken waardoor een afbeelding tevoorschijn 
 
+// flipcard maken met site: https://jefferson-cuartas.medium.com/how-to-create-a-flip-card-effect-using-javascript-767dd945210c#:~:text=Firstly%2C%20in%20your%20JS%20index,which%20toggles%20on%20a%20click.
+// const card = document.querySelector(".card")
 
-  // flipcard maken met site: https://jefferson-cuartas.medium.com/how-to-create-a-flip-card-effect-using-javascript-767dd945210c#:~:text=Firstly%2C%20in%20your%20JS%20index,which%20toggles%20on%20a%20click.
-  const card = document.querySelector(".card")
+// card.addEventListener("click", flipCard);
 
-  card.addEventListener("click", flipCard);
-
-  function flipCard(front, back) {
-    card.classList.toggle("flipCard")
-  }
+// function flipCard(front, back) {
+//   card.classList.toggle("flipCard")
+// }
